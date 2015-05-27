d7898500380034b28955641024b6ee0847dd7f73
Antonu Temian <t_tony_182@yahoo.com>
2015-05-27 21:08:38 +0300

    initial commit

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_NAME', 'catalog');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_HOST', 'localhost');
+
+define('BASE_PATH','http://www.catalog.ro');
+
+define ('DEVELOPMENT_ENVIRONMENT', true);
