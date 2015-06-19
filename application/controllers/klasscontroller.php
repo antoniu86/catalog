dcfed5af293a21727934c142570dc9853ece4fed
Antonu Temian <t_tony_182@yahoo.com>
2015-06-20 01:00:17 +0300

    prepare to add classes support - with k

---
+++ application/controllers/klasscontroller.php
@@ -0,0 +1,7 @@
+<?php
+
+class KlassController extends ApplicationController {
+    
+    
+    
+}
---
+++ application/controllers/klassescontroller.php
@@ -0,0 +1,7 @@
+<?php
+
+class KlassesController extends ApplicationController {
+    
+    
+    
+}
